55db6528e1b0e5a2163c3ada879969deac6a11eb
Jeffry Saputra <jeffrysaputra97@yahoo.com>
2018-08-02 16:13:11 +0700

    Menyempurnakan mekanisme penyimpanan konfigurasi sederhana dengan menggunakan SharedPreferences.

--- app/src/main/java/com/ithb/jeffry/myalarmmanager/AlarmPreference.java
+++ app/src/main/java/com/ithb/jeffry/myalarmmanager/AlarmPreference.java
@@ -27,6 +27,42 @@ public class AlarmPreference {
         return mSharedPreferences.getString(KEY_ONE_TIME_DATE, null);
     }
 
+    public void setOneTimeTime(String time){
+        editor.putString(KEY_ONE_TIME_TIME, time);
+        editor.commit();
+    }
+
+    public String getOneTimeTime(){
+        return mSharedPreferences.getString(KEY_ONE_TIME_TIME, null);
+    }
+
+    public void setOneTimeMessage(String message){
+        editor.putString(KEY_ONE_TIME_MESSAGE, message);
+        editor.commit();
+    }
+
+    public String getOneTimeMessage(){
+        return mSharedPreferences.getString(KEY_ONE_TIME_MESSAGE, null);
+    }
+
+    public void setRepeatingTime(String time){
+        editor.putString(KEY_REPEATING_TIME, time);
+        editor.commit();
+    }
+
+    public String getRepeatingTime(){
+        return mSharedPreferences.getString(KEY_REPEATING_TIME, null);
+    }
+
+    public void setRepeatingMessage(String message){
+        editor.putString(KEY_REPEATING_MESSAGE, message);
+        editor.commit();
+    }
+
+    public String getRepeatingMessage(){
+        return mSharedPreferences.getString(KEY_REPEATING_MESSAGE, null);
+    }
+
     public void clear(){
         editor.clear();
         editor.commit();
